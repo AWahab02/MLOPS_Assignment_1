bb76e7d4614d8a18ebdc63dccf74736535b2dcd5
AWahab02 <awzahid02@gmail.com>
2024-03-25 21:59:38 +0500

    workflows added to dev

--- app.py
+++ app.py
@@ -5,11 +5,12 @@ import numpy as np
 app = Flask(__name__)
 
 model = joblib.load('model.pkl')
+
+
 @app.route('/', methods=['GET'])
-
-
 def me():
     return "Hello World"
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -17,6 +18,7 @@ def predict():
     features = np.array(data['features']).reshape(1, -1)
     prediction = model.predict(features)[0]
     return jsonify({'prediction': prediction})
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
--- main.py
+++ main.py
@@ -4,17 +4,17 @@ from sklearn.linear_model import LinearRegression
 import joblib
 
 salary_url = ""
-"https://github.com/ybifoundation/Dataset/raw/main/Salary%20Data.csv"
-""
+"https://github.com/ybifoundation/Dataset/raw/main/Salary%20Data.csv"""
+
 salary = pd.read_csv(salary_url)
 
 X = salary[['Experience Years']]
 y = salary['Salary']
 
 X_train, X_test, y_train, y_test = train_test_split(X,
-y, 
-test_size=0.2, 
-random_state=42)
+                                                    y,
+                                                    test_size=0.2,
+                                                    random_state=42)
 
 model = LinearRegression()
 model.fit(X_train, y_train)
