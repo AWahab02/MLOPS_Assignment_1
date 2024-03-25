31dad53165834360d58176579ad9a8373045a4ba
AWahab02 <awzahid02@gmail.com>
2024-03-25 21:39:48 +0500

    main.py modified

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 model = LinearRegression()
 model.fit(X_train, y_train)
 
-
+print('hello')
 
 joblib.dump(model, 'model.pkl')
 
